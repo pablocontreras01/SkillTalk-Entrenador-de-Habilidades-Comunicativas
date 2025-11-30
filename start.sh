4d315e502cf845fcad16bbbfb70c67c868ad9d4e
pablocontreras01 <pablo.contreras01@alumnos.ucn.cl>
2025-11-30 01:16:02 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ export STREAMLIT_SERVER_PORT=${PORT:-8501}
 export STREAMLIT_SERVER_ADDRESS=0.0.0.0
 
 # Asegúrate de que el archivo principal se llama modelo_final_skilltalk.py
-exec streamlit run modelo_final_skilltalk.py --server.port $STREAMLIT_SERVER_PORT --server.address $STREAMLIT_SERVER_ADDRESS
+exec streamlit run app.py --server.port $STREAMLIT_SERVER_PORT --server.address $STREAMLIT_SERVER_ADDRESS
